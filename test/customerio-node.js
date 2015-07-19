48aa10a6d95c25848a533362ee9c6630fb342db4
Alvin Crespo <alvin.crespo@gmail.com>
2015-07-19 11:39:29 -0400

    Moves URI generation to the client
    - Also creates test for the options method on the Request class

--- test/customerio-node.js
+++ test/customerio-node.js
@@ -23,47 +23,51 @@ describe('#constructor', function() {
 describe('#identify', function() {
   it('makes a PUT request', function() {
     let cio = new CIO(123, 'abc');
+    let uri = cio.uri(1);
 
     sinon.stub(cio.request, 'put');
 
     cio.identify(1);
 
-    assert.ok(cio.request.put.calledWith(1, {}));
+    assert.ok(cio.request.put.calledWith(uri, {}));
   });
 });
 
 describe('#destroy', function() {
   it('makes a DELETE request', function() {
     let cio = new CIO(123, 'abc');
+    let uri = cio.uri(1);
 
     sinon.stub(cio.request, 'destroy');
 
     cio.destroy(1);
 
-    assert.ok(cio.request.destroy.calledWith(1));
+    assert.ok(cio.request.destroy.calledWith(uri));
   });
 });
 
 describe('#track', function() {
   it('makes a POST request', function() {
     let cio = new CIO(123, 'abc');
+    let uri = `${cio.uri(1)}/events`;
 
     sinon.stub(cio.request, 'post');
 
     cio.track(1);
 
-    assert.ok(cio.request.post.calledWith(1, {}));
+    assert.ok(cio.request.post.calledWith(uri, {}));
   });
 });
 
 describe('#trackPageView', function() {
   it('makes a POST request', function() {
     let cio = new CIO(123, 'abc');
+    let uri = cio.uri(1);
 
     sinon.stub(cio.request, 'post');
 
     cio.trackPageView(1, '#home');
 
-    assert.ok(cio.request.post.calledWith(1, { type: 'page', name: '#home'}));
+    assert.ok(cio.request.post.calledWith(uri, { type: 'page', name: '#home'}));
   });
 });
--- test/request.js
+++ test/request.js
@@ -1,3 +1,4 @@
+let assert = require('chai').assert;
 let should = require('chai').should();
 let Request = require('../lib/request');
 
@@ -14,6 +15,26 @@ describe('#constructor', function() {
     let auth = 'Basic ' + new Buffer(`${request.siteid}:${request.apikey}`).toString('base64');
 
     request.auth.should.equal(auth);
+  });
+});
+
+describe('#options', function() {
+  it('returns an Object of options to be passed to the request', function() {
+    let request = new Request(123, 'abc');
+
+    let uri = 'https://track.customer.io/api/v1/customers/1';
+    let auth = 'Basic ' + new Buffer(`${request.siteid}:${request.apikey}`).toString('base64');
+
+    let expectedOptions = {
+      headers: { Authorization: auth },
+      uri,
+      timeout: 10000,
+      method: 'POST'
+    }
+
+    let resultOptions = request.options(uri, 'POST');
+
+    assert.deepEqual(resultOptions, expectedOptions);
   });
 });
 
