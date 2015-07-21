1cb96a7944f4afa448479b2e924c170dd5f5912f
Alvin Crespo <alvin.crespo@gmail.com>
2015-07-21 19:51:25 -0400

    Wrapping cio tests with main describe
    - Also caches the CIO instance

--- test/customerio-node.js
+++ test/customerio-node.js
@@ -3,71 +3,71 @@ var should = require('chai').should();
 var sinon = require('sinon');
 var CIO = require('../lib/customerio-node');
 
-describe('#constructor', function() {
-  it('sets siteid and apikey', function() {
-    var cio = new CIO(123, 'abc');
+describe('CustomerIO', function(){
+  var cio;
 
-    cio.siteid.should.equal(123);
-    cio.apikey.should.equal('abc');
+  beforeEach(function() {
+    cio = new CIO(123, 'abc');
   });
 
-  it('creates a request object that stores the siteid and apikey', function() {
-    var cio = new CIO(123, 'abc');
+  describe('#constructor', function() {
+    it('sets siteid and apikey', function() {
+      cio.siteid.should.equal(123);
+      cio.apikey.should.equal('abc');
+    });
 
-    assert.ok(cio.request);
-    cio.request.siteid.should.equal(123);
-    cio.request.apikey.should.equal('abc');
+    it('creates a request object that stores the siteid and apikey', function() {
+      assert.ok(cio.request);
+      cio.request.siteid.should.equal(123);
+      cio.request.apikey.should.equal('abc');
+    });
   });
-});
 
-describe('#identify', function() {
-  it('makes a PUT request', function() {
-    var cio = new CIO(123, 'abc');
-    var uri = cio.uri(1);
+  describe('#identify', function() {
+    it('makes a PUT request', function() {
+      var uri = cio.uri(1);
 
-    sinon.stub(cio.request, 'put');
+      sinon.stub(cio.request, 'put');
 
-    cio.identify(1);
+      cio.identify(1);
 
-    assert.ok(cio.request.put.calledWith(uri, {}));
+      assert.ok(cio.request.put.calledWith(uri, {}));
+    });
   });
-});
 
-describe('#destroy', function() {
-  it('makes a DEvarE request', function() {
-    var cio = new CIO(123, 'abc');
-    var uri = cio.uri(1);
+  describe('#destroy', function() {
+    it('makes a DEvarE request', function() {
+      var uri = cio.uri(1);
 
-    sinon.stub(cio.request, 'destroy');
+      sinon.stub(cio.request, 'destroy');
 
-    cio.destroy(1);
+      cio.destroy(1);
 
-    assert.ok(cio.request.destroy.calledWith(uri));
+      assert.ok(cio.request.destroy.calledWith(uri));
+    });
   });
-});
 
-describe('#track', function() {
-  it('makes a POST request', function() {
-    var cio = new CIO(123, 'abc');
-    var uri = cio.uri(1) + 'events';
+  describe('#track', function() {
+    it('makes a POST request', function() {
+      var uri = cio.uri(1) + 'events';
 
-    sinon.stub(cio.request, 'post');
+      sinon.stub(cio.request, 'post');
 
-    cio.track(1);
+      cio.track(1);
 
-    assert.ok(cio.request.post.calledWith(uri, {}));
+      assert.ok(cio.request.post.calledWith(uri, {}));
+    });
   });
-});
 
-describe('#trackPageView', function() {
-  it('makes a POST request', function() {
-    var cio = new CIO(123, 'abc');
-    var uri = cio.uri(1) + 'events';
+  describe('#trackPageView', function() {
+    it('makes a POST request', function() {
+      var uri = cio.uri(1) + 'events';
 
-    sinon.stub(cio.request, 'post');
+      sinon.stub(cio.request, 'post');
 
-    cio.trackPageView(1, '#home');
+      cio.trackPageView(1, '#home');
 
-    assert.ok(cio.request.post.calledWith(uri, { type: 'page', name: '#home'}));
+      assert.ok(cio.request.post.calledWith(uri, { type: 'page', name: '#home'}));
+    });
   });
 });
