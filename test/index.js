0d2e9328663370655e6fa471e663223a0616e15f
Jeff Escalante <gh.je@mailhero.io>
2018-03-13 14:59:50 -0400

    add trackAnonymous method, correct api base for triggerBroadcast

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@ const test = require('ava')
 const sinon = require('sinon')
 const CIO = require('../lib')
 
-const base = 'https://track.customer.io/api/v1'
+const trackRoot = 'https://track.customer.io/api/v1'
+const apiRoot = 'https://api.customer.io/v1/api'
 
 test.beforeEach(t => {
   t.context.client = new CIO(123, 'abc')
@@ -19,30 +20,39 @@ test('constructor sets necessary variables', t => {
 test('#identify works', t => {
   sinon.stub(t.context.client.request, 'put')
   t.context.client.identify(1)
-  t.truthy(t.context.client.request.put.calledWith(`${base}/customers/1`, {}))
+  t.truthy(
+    t.context.client.request.put.calledWith(`${trackRoot}/customers/1`, {})
+  )
 })
 
 test('#destroy works', t => {
   sinon.stub(t.context.client.request, 'destroy')
   t.context.client.destroy(1)
-  t.truthy(t.context.client.request.destroy.calledWith(`${base}/customers/1`))
+  t.truthy(
+    t.context.client.request.destroy.calledWith(`${trackRoot}/customers/1`)
+  )
 })
 
 test('#track with customer id works', t => {
   sinon.stub(t.context.client.request, 'post')
   t.context.client.track(1, { data: 'yep' })
   t.truthy(
-    t.context.client.request.post.calledWith(`${base}/customers/1/events`, {
-      data: 'yep'
-    })
+    t.context.client.request.post.calledWith(
+      `${trackRoot}/customers/1/events`,
+      {
+        data: 'yep'
+      }
+    )
   )
 })
 
-test('#track without customer id works', t => {
+test('#trackAnonymous works', t => {
   sinon.stub(t.context.client.request, 'post')
-  t.context.client.track({ data: 'yep' })
+  t.context.client.trackAnonymous({ data: 'yep' })
   t.truthy(
-    t.context.client.request.post.calledWith(`${base}/events`, { data: 'yep' })
+    t.context.client.request.post.calledWith(`${trackRoot}/events`, {
+      data: 'yep'
+    })
   )
 })
 
@@ -50,10 +60,13 @@ test('#trackPageView works', t => {
   sinon.stub(t.context.client.request, 'post')
   t.context.client.trackPageView(1, '#home')
   t.truthy(
-    t.context.client.request.post.calledWith(`${base}/customers/1/events`, {
-      type: 'page',
-      name: '#home'
-    })
+    t.context.client.request.post.calledWith(
+      `${trackRoot}/customers/1/events`,
+      {
+        type: 'page',
+        name: '#home'
+      }
+    )
   )
 })
 
@@ -61,9 +74,12 @@ test('#triggerBroadcast works', t => {
   sinon.stub(t.context.client.request, 'post')
   t.context.client.triggerBroadcast(1, { type: 'data' }, { type: 'recipients' })
   t.truthy(
-    t.context.client.request.post.calledWith(`${base}/campaigns/1/triggers`, {
-      data: { type: 'data' },
-      recipients: { type: 'recipients' }
-    })
+    t.context.client.request.post.calledWith(
+      `${apiRoot}/campaigns/1/triggers`,
+      {
+        data: { type: 'data' },
+        recipients: { type: 'recipients' }
+      }
+    )
   )
 })
