362e86c1a371b837d98e544f84057b5852720973
Swastik Agarwal <swastik@customer.io>
2020-11-24 12:16:15 +0530

    Add a test for the api sendEmail call

---
+++ test/api.js
@@ -0,0 +1,21 @@
+const test = require('ava');
+const sinon = require('sinon');
+const CioApiClient = require('../lib/api');
+
+const apiRoot = 'https://api.customer.io/v1';
+
+test.beforeEach((t) => {
+  t.context.client = new CioApiClient('appkey');
+});
+
+test('constructor sets necessary variables', (t) => {
+  t.is(t.context.client.appkey, 'appkey');
+  t.truthy(t.context.client.request);
+});
+
+test('#sendEmail works', (t) => {
+  sinon.stub(t.context.client.request, 'post');
+  let payload = { customer_id: '2', transactional_message_id: 1 }
+  t.context.client.sendEmail(payload);
+  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, payload));
+});
