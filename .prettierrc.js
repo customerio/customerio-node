3466ec245bf079b6e8d2f6224c65d8f893bc9832
Swastik Agarwal <swastik@customer.io>
2020-11-24 11:50:13 +0530

    Add initial support for the transactional API

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: ['*.js', '.*.js'],
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
+};
