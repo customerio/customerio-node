3996aa21c40309f19c3ef02af27fd8a9bc92b396
Swastik Agarwal <swastik@customer.io>
2020-11-27 10:03:11 +0530

    Fix naming

--- test/api.js
+++ test/api.js
@@ -5,17 +5,17 @@ const CioApiClient = require('../lib/api');
 const apiRoot = 'https://api.customer.io/v1';
 
 test.beforeEach((t) => {
-  t.context.client = new CioApiClient('appkey');
+  t.context.client = new CioApiClient('appKey');
 });
 
 test('constructor sets necessary variables', (t) => {
-  t.is(t.context.client.appkey, 'appkey');
+  t.is(t.context.client.appKey, 'appKey');
   t.truthy(t.context.client.request);
 });
 
 test('#sendEmail works', (t) => {
   sinon.stub(t.context.client.request, 'post');
-  let payload = { customer_id: '2', transactional_message_id: 1 }
+  let payload = { customer_id: '2', transactional_message_id: 1 };
   t.context.client.sendEmail(payload);
   t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, payload));
 });
