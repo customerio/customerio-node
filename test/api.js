d431d8cd3cdf07239d49398364c4dd787ac1d160
Swastik Agarwal <swastik@customer.io>
2020-12-01 12:35:16 +0530

    Change how things work internally: just wrap the message in the class

--- test/api.js
+++ test/api.js
@@ -17,31 +17,31 @@ test('constructor sets necessary variables', (t) => {
 
 test('sendEmail: passing in a plain object throws an error', (t) => {
   sinon.stub(t.context.client.request, 'post');
-  let message = { identifiers: { id: '2' }, transactional_message_id: 1 };
-  t.throws(() => t.context.client.sendEmail(message), {
-    message: /"message" must be an instance of SendEmailRequest/,
+  let req = { identifiers: { id: '2' }, transactional_message_id: 1 };
+  t.throws(() => t.context.client.sendEmail(req), {
+    message: /"request" must be an instance of SendEmailRequest/,
   });
   t.falsy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`));
 });
 
 test('#sendEmail: success', (t) => {
   sinon.stub(t.context.client.request, 'post');
-  let message = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
-  t.context.client.sendEmail(message);
-  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, message.toObject()));
+  let req = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
+  t.context.client.sendEmail(req);
+  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, req.message));
 });
 
 test('#sendEmail: adding attachments using a buffer object', (t) => {
   let buf = Buffer.from('hello world!');
 
   sinon.stub(t.context.client.request, 'post');
-  let message = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
+  let req = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
 
-  message.attach('test', buf);
-  t.truthy(message.attachments.test, buf.toString('base64'));
+  req.attach('test', buf);
+  t.truthy(req.message.attachments.test, buf.toString('base64'));
 
-  t.context.client.sendEmail(message);
-  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, message.toObject()));
+  t.context.client.sendEmail(req);
+  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, req.message));
 });
 
 test('#sendEmail: adding attachments using filepath', (t) => {
@@ -49,33 +49,33 @@ test('#sendEmail: adding attachments using filepath', (t) => {
   sinon.stub(fs, 'readFileSync').withArgs('test.pdf', 'base64').returns(buf.toString('base64'));
   sinon.stub(t.context.client.request, 'post');
 
-  let message = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
+  let req = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
 
-  message.attach('file', 'test.pdf');
-  t.truthy(message.attachments.file, buf.toString('base64'));
+  req.attach('file', 'test.pdf');
+  t.truthy(req.message.attachments.file, buf.toString('base64'));
 
-  t.context.client.sendEmail(message);
-  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, message.toObject()));
+  t.context.client.sendEmail(req);
+  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, req.message));
 });
 
 test('#sendEmail: adding unknown attachments', (t) => {
   sinon.stub(t.context.client.request, 'post');
-  let message = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
+  let req = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
 
-  t.throws(() => message.attach('file', {}), { message: /unknown attachment type/ });
+  t.throws(() => req.attach('file', {}), { message: /unknown attachment type/ });
 
-  t.context.client.sendEmail(message);
-  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, message.toObject()));
+  t.context.client.sendEmail(req);
+  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, req.message));
 });
 
 test('#sendEmail: error', async (t) => {
   sinon.stub(t.context.client.request, 'post').rejects({ message: 'sample error', statusCode: 400 });
 
-  let message = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
-  t.context.client.sendEmail(message).catch((err) => {
+  let req = new SendEmailRequest({ identifiers: { id: '2' }, transactional_message_id: 1 });
+  t.context.client.sendEmail(req).catch((err) => {
     t.is(err.message, 'sample error');
     t.is(err.statusCode, 400);
   });
 
-  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, message.toObject()));
+  t.truthy(t.context.client.request.post.calledWith(`${apiRoot}/send/email`, req.message));
 });
