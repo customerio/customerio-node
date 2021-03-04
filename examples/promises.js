5c93a7e2c5a9e610d8385884abb723f68bfea22e
glosier <glosier@customer.io>
2021-03-04 11:04:05 -0800

    updated examples

--- examples/promises.js
+++ examples/promises.js
@@ -1,9 +1,11 @@
-let CIO = require('../lib/index');
+let CIO = require('../lib/track.js');
 // In actual use require the node module: let CIO = require('customerio-node');
 const siteId = require('./config').siteId;
 const apiKey = require('./config').apiKey;
 const customerId = require('./config').customerId;
-const cio = new CIO(siteId, apiKey);
+// In actual use, specify your specific region and require node module: const { RegionUS, RegionEU } = require('customerio-node/regions')
+const { RegionUS, RegionEU } = require('../lib/regions');
+const cio = new CIO(siteId, apiKey, { region: RegionUS });
 
 cio
   .identify(customerId, {
