e5de75c5fa72b99bb518a72c6c1b0edf117b511b
Mike Engel <engel@customer.io>
2022-04-20 15:36:18 +0200

    fix: fix script

--- update-version.js
+++ update-version.js
@@ -2,4 +2,4 @@ const packageJson = require('./package.json');
 const { resolve } = require('path');
 const { writeFileSync } = require('fs');
 
-writeFileSync(resolve(__dirname, '..', 'lib/version.ts'), `export const version = '${packageJson.version}';\n`);
+writeFileSync(resolve(__dirname, 'lib/version.ts'), `export const version = '${packageJson.version}';\n`);
