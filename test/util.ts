f921e9a140b6f8f01c68ca35526028674e975eb5
sydneycollins-cio <149523950+sydneycollins-cio@users.noreply.github.com>
2026-05-21 13:22:46 -0400

    CDP-5966: remove cleanEmail tests (helper removed)

--- test/util.ts
+++ test/util.ts
@@ -1,20 +1,5 @@
 import avaTest, { TestFn } from 'ava';
-import { cleanEmail } from '../lib/utils';
 
 type TestContext = {};
 
 const test = avaTest as TestFn<TestContext>;
-
-test('#cleanEmail correctly formats email (default)', (t) => {
-  let email = 'hello+test@world.com';
-  let result = cleanEmail(email);
-  t.is(result, 'hello%2Btest@world.com');
-
-  email = 'test@world.com';
-  result = cleanEmail(email);
-  t.is(result, 'test@world.com');
-
-  email = '';
-  result = cleanEmail(email);
-  t.is(result, '');
-});
